f17cbc83a802bc543dd3e29a4ad731e26199f32e
earvin <earvinjtallod@gmail.com>
2020-03-26 13:13:22 +0800

    user_interface

---
+++ resources/views/layouts/include/main_nav.blade.php
@@ -0,0 +1,21 @@
+<aside id="menubar" class="menubar light">
+  <div class="menubar-scroll">
+    <div class="menubar-scroll-inner">
+      <ul class="app-menu">
+        <li class="has-submenu">
+          <a href="javascript:void(0)" class="submenu-toggle">
+            <i class="menu-icon zmdi zmdi-view-dashboard zmdi-hc-lg"></i>
+            <span class="menu-text">Pages Management</span>
+            <i class="menu-caret zmdi zmdi-hc-sm zmdi-chevron-right"></i>
+          </a>
+          <ul class="submenu">
+            <li><a href="index.html"><span class="menu-text">View All</span></a></li>
+            <li><a href="dashboard.2.html"><span class="menu-text">Add New</span></a></li>
+            <li><a href="dashboard.3.html"><span class="menu-text">Media</span></a></li>
+            <li><a href="dashboard.3.html"><span class="menu-text">Categories</span></a></li>
+          </ul>
+        </li>
+      </ul><!-- .app-menu -->
+    </div><!-- .menubar-scroll-inner -->
+  </div><!-- .menubar-scroll -->
+</aside>
